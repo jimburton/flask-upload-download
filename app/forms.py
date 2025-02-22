33e2261c71bc35257db45791a1632bd904c6c5df
James Burton <jimburton1@gmail.com>
2025-02-22 13:28:08 +0000

    Removed unused templates.

--- app/forms.py
+++ app/forms.py
@@ -15,10 +15,10 @@ class MenuForm(FlaskForm):
 
     def __init__(self, menu_items, *args, **kwargs):
         super(MenuForm, self).__init__(*args, **kwargs)
-        starter_choices= [(name,f'{name} ({price})') for [name,price] in menu_items['Starter']]
-        main_choices= [(name,f'{name} ({price})') for [name,price] in menu_items['Main']]
-        desert_choices= [(name,f'{name} ({price})') for [name,price] in menu_items['Desert']]
+        starter_choices = [(name,f'{name} ({price})') for [name,price] in menu_items['Starter']]
+        main_choices    = [(name,f'{name} ({price})') for [name,price] in menu_items['Main']]
+        desert_choices  = [(name,f'{name} ({price})') for [name,price] in menu_items['Desert']]
         self.starter.choices = starter_choices
-        self.main.choices = main_choices
-        self.desert.choices = desert_choices
+        self.main.choices    = main_choices
+        self.desert.choices  = desert_choices
         
